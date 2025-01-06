00c2ffaf0f5a2d40d9dac2985738937cf3284143
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-01-06 12:32:50 +0100

    add

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found out" });
+  res.status(404).json({ message: "Not found outyyy" });
 });
 
 app.use((err, req, res, next) => {
