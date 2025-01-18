a21a3e50cc5dafbc3f3a2bd59bf37865bf380fe2
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-01-18 17:46:18 +0100

    add

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
