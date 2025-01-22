d9564c11f6cac1cfa4e37972ba06e541da3e1d28
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-01-22 18:18:25 +0100

    add

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,28 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/users");
 
-const authenticate = async (req, res, next) => {
-  const { authorization } = req.headers;
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-  if (!authorization) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Not authorized" });
   }
 
-  const token = authorization.replace("Bearer ", "");
+  const token = authHeader.split(" ")[1];
+
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Zweryfikuj token
     const user = await User.findById(decoded.id);
 
     if (!user || user.token !== token) {
       return res.status(401).json({ message: "Not authorized" });
     }
 
-    req.user = user;
+    req.user = user; // Zapisz dane użytkownika w żądaniu
     next();
   } catch (error) {
-    res.status(401).json({ message: "Not authorized" });
+    return res.status(401).json({ message: "Not authorized" });
   }
 };
 
-module.exports = { authenticate };
+module.exports = authMiddleware;
