e71e89434c2de6f2c1c1443996c4f37a29c6a9f9
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-01-25 21:43:27 +0100

    add

--- models/user.js
+++ models/user.js
@@ -1,4 +1,52 @@
-import mongoose, { Schema } from "mongoose";
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+// Hashowanie hasła przed zapisaniem
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model("user", userSchema);
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  favorite: { type: Boolean, default: false },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+});
+
+const Contact = mongoose.model("contact", contactSchema);
+
+module.exports = { User, Contact };
 
 // Add ADMIN role via Database Client tools.
 export const UserRole = Object.freeze({
