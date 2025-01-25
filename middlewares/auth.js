8a3728ef69cdb09d8ffa444a52dede11dc59feea
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-01-26 00:21:53 +0100

    add

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,16 @@
 import { JWT } from "../lib/jwt.js";
-import { User, UserRole } from "../models/user.js";
+import { Users, UserRole } from "../models/Users.js";
 import { HttpError } from "../models/HttpError.js";
 
 export const auth =
   (roles = [UserRole.USER, UserRole.ADMIN]) =>
   async (req, res, next) => {
-    const AuthorizationHeader = req.headers["authorization"];
-    const token = AuthorizationHeader?.replace("Bearer ", "");
+    /* Auth Header */
+    // const authtoken = req.headers["authorization"];
+    /* Auth Cookie */
+    const authtoken = req.cookies["authorization"];
+
+    const token = authtoken?.replace("Bearer ", "");
 
     const isTokenValid = await JWT.verify(token);
     if (!isTokenValid) {
@@ -15,12 +19,12 @@ export const auth =
     }
 
     const tokenData = JWT.decode(token);
-    if (!tokenData?.data?.id) {
+    if (!tokenData?.data) {
       // return res.status(401).json({ error: "Malformed token." });
       return next(new HttpError(401, "Malformed token."));
     }
 
-    const user = await User.findById(tokenData?.data?.id);
+    const user = await Users.findById(tokenData?.data?.id);
 
     if (!user || !roles.includes(user?.role)) {
       // return res.status(403).json({ error: "Unauthorized." });
