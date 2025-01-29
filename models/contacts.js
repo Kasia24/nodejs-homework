128c22926827e9988633a270ec8886c3fa222dba
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-01-29 09:17:42 +0100

    add

--- models/contacts.js
+++ models/contacts.js
@@ -1,43 +1,19 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const contactSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: [true, "Set name for contact"],
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [
-        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-        "Please fill a valid email address",
-      ],
-    },
-    phone: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/^\(\d{3}\) \d{3}-\d{4}$/, "Please provide a valid phone number"],
-    },
-    favorite: {
-      type: Boolean,
-      default: false,
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true, unique: true },
+    favorite: { type: Boolean, default: false },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
   },
-  { timestamps: true } // Automatyczne dodanie `createdAt` i `updatedAt`
+  { timestamps: true }
 );
 
-// Dodanie metod i funkcji statycznych
-contactSchema.methods.markAsFavorite = function () {
-  this.favorite = true;
-  return this.save();
-};
+const Contact = mongoose.model("Contact", contactSchema);
 
-contactSchema.statics.findFavorites = function () {
-  return this.find({ favorite: true });
-};
-
-module.exports = mongoose.model("Contact", contactSchema);
+export default Contact;
