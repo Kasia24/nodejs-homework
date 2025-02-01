bee97d30a2bfdcc1ecda717c06c6732bb928e047
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-02-01 21:19:57 +0100

    add

---
+++ models/contacts.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true, unique: true },
+    favorite: { type: Boolean, default: false },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Contact = mongoose.model("Contact", contactSchema);
+module.exports = Contact;
