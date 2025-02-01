b8b215c5f113b9e87fad909cb4180bc23196741d
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-02-01 21:37:42 +0100

    add

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useFindAndModify: false, // Jeśli używasz findAndModify, ale ta opcja już jest niezalecana
+      useCreateIndex: true,
     });
     console.log(`Database connected: ${conn.connection.host}`);
   } catch (error) {
