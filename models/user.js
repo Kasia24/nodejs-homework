0c1e414ef0612d292c0e92a1a532ae96e5fb7e7d
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-02-01 13:40:05 +0100

    add

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const gravatar = require("gravatar");
 
 const userSchema = new mongoose.Schema(
   {
@@ -23,6 +24,18 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  if (this.isNew || this.isModified("email")) {
+    const avatar = gravatar.url(this.email, {
+      s: "200", // Rozmiar awatara
+      r: "pg", // Jakość (pg = rysunki)
+      d: "identicon", // Domyślny awatar (jeśli użytkownik nie ma Gravatar)
+    });
+    this.avatarURL = avatar;
+  }
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
