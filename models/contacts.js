a623e0b89f511d15e8a14b67b7ba91fc93630c29
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-02-02 00:38:19 +0100

    add

--- models/contacts.js
+++ models/contacts.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const gravatar = require("gravatar");
 
 const contactSchema = new mongoose.Schema(
   {
@@ -6,7 +7,12 @@ const contactSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: true, unique: true },
     favorite: { type: Boolean, default: false },
-    avatarURL: { type: String, default: "" }, // Dodajemy pole avatarURL
+    avatarURL: {
+      type: String,
+      default: function () {
+        return gravatar.url(this.email, { s: "250", d: "retro" }, true);
+      },
+    },
     token: {
       type: String,
       default: null,
